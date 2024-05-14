ebd0dc012f157e44121fa9b334081c7e6b3a9cdf
Falllll <falldyshinoda@gmail.com>
2024-05-14 15:13:35 +0700

    fight monster

--- js/script.js
+++ js/script.js
@@ -74,6 +74,12 @@ const locations = [
         "button text": ["Attack", "Dodge", "Run"],
         "button functions": [attack, dodge, goTown],
         text: "You are fighting a monster."
+    },
+    {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     }
 ];
 
@@ -83,6 +89,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+    monsterStats.style.display = "none";
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
@@ -164,12 +171,51 @@ function fightDragon() {
 
 function goFight(){
     update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = 'block';
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsters[fighting].health;
 }
 
 function attack(){
-
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp); 
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if(health <= 0){
+        lose();
+    }else if(monsterHealth <= 0){
+        defeatMonster();
+    }
 }
 
 function dodge(){
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
 
+function defeatMonster(){
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    locations[4];
+}
+
+function lose(){
+    update(locations[5]);
+}
+
+function restart(){
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ["stick"];
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+    goTown();
 }
